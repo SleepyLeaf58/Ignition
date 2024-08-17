ec9438a24b7612534c6ecc93df741babcf7f7859
david <davidzhai07@gmail.com>
2024-08-17 12:39:55 -0400

    fixed bug with UpgradeScreen

--- src/game/main.js
+++ src/game/main.js
@@ -2,6 +2,7 @@ import { Boot } from './scenes/Boot';
 import { Game } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
+import{UpgradeScreen} from'./scenes/UpgradeScreen';
 import Phaser from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
